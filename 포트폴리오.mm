ed9f9703a28bb135bad2bd4286c24f68525a9db6
dhlee702 <dhlee702@naver.com>
2025-12-05 15:00:00 +0900

    "fix css problem"

---
+++ 포트폴리오.mm
@@ -0,0 +1,172 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="포트폴리오" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1764899027759" STYLE="oval">
+<font NAME="맑은 고딕" SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" associatedTemplateLocation="template:/standard-1.6.mm" followedTemplateLocation="template:/standard-1.6.mm" followedMapLastTime="1734928862149"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#000000" STYLE="fork" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" TEXT_WRITING_DIRECTION="LEFT_TO_RIGHT" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#808080" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="SOLID" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt" CHILD_NODES_LAYOUT="AUTO">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="맑은 고딕" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="바이브코딩" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="bottom_or_right" ID="ID_12267700" CREATED="1764899028700" MODIFIED="1764899050451">
+<edge COLOR="#ff0000"/>
+<node TEXT="0. User Rules" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_751691459" CREATED="1764899055783" MODIFIED="1764901106793">
+<node TEXT="구현 요청 프롬프트" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" FOLDED="true" ID="ID_391618697" CREATED="1764899110273" MODIFIED="1764900674889">
+<node TEXT="#역할" ID="ID_73983691" CREATED="1764899122269" MODIFIED="1764899153930">
+<node TEXT="당신은 20년 이상 웹 서비스를 개발하는 개발자입니다." ID="ID_1414112746" CREATED="1764899193437" MODIFIED="1764900732740"/>
+</node>
+<node TEXT="# 지시사항" ID="ID_1506503792" CREATED="1764899217703" MODIFIED="1764899223218">
+<node TEXT="지금까지 구현한 내용을 당신이 파악하기 위해서 전체 코드베이스를 읽어서 구현 내용을 파악해 주세요." ID="ID_1714523607" CREATED="1764899224095" MODIFIED="1764900777787"/>
+</node>
+<node TEXT="# 상세지침" ID="ID_1365595343" CREATED="1764899217703" MODIFIED="1764900820664">
+<node TEXT="파일의 요구사항 명세서를 참고해서 포트폴리오 웹사이틀 개발해주세요.&#xa;Layout과 배경색상, 타이포 중심으로 세련되게 개발해주시고, 이미지는 정말 필요한 부분만 적용해주세요.&#xa;배경 색상은 민트 색상톤으로 개발해주세요." ID="ID_395405039" CREATED="1764899224095" MODIFIED="1764900914729"/>
+</node>
+</node>
+</node>
+<node TEXT="0. Project Rules" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_566460094" CREATED="1764899055783" MODIFIED="1764901135313">
+<node TEXT="구현 요청 프롬프트" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" FOLDED="true" ID="ID_675650392" CREATED="1764899110273" MODIFIED="1764900674889">
+<node TEXT="#역할" ID="ID_1036188901" CREATED="1764899122269" MODIFIED="1764899153930">
+<node TEXT="당신은 20년 이상 웹 서비스를 개발하는 개발자입니다." ID="ID_985820442" CREATED="1764899193437" MODIFIED="1764900732740"/>
+</node>
+<node TEXT="# 지시사항" ID="ID_1064891060" CREATED="1764899217703" MODIFIED="1764899223218">
+<node TEXT="지금까지 구현한 내용을 당신이 파악하기 위해서 전체 코드베이스를 읽어서 구현 내용을 파악해 주세요." ID="ID_1134591949" CREATED="1764899224095" MODIFIED="1764900777787"/>
+</node>
+<node TEXT="# 상세지침" ID="ID_225437655" CREATED="1764899217703" MODIFIED="1764900820664">
+<node TEXT="파일의 요구사항 명세서를 참고해서 포트폴리오 웹사이틀 개발해주세요.&#xa;Layout과 배경색상, 타이포 중심으로 세련되게 개발해주시고, 이미지는 정말 필요한 부분만 적용해주세요.&#xa;배경 색상은 민트 색상톤으로 개발해주세요." ID="ID_194446807" CREATED="1764899224095" MODIFIED="1764900914729"/>
+</node>
+</node>
+</node>
+<node TEXT="1. 요구사항 명세서&#xa;   작성" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1136753787" CREATED="1764899055783" MODIFIED="1764900641651">
+<node TEXT="chatgpt에게 작성요청" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_938708156" CREATED="1764899089124" MODIFIED="1764899109612">
+<node TEXT="작성결과는 Readme.md에  요구사항 명세서로서 저장" ID="ID_447301207" CREATED="1764900602214" MODIFIED="1764900624570"/>
+</node>
+<node TEXT="작성 요청 프롬프트" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1663536729" CREATED="1764899110273" MODIFIED="1764899134486">
+<node TEXT="#역할" ID="ID_837396643" CREATED="1764899122269" MODIFIED="1764899153930">
+<node TEXT="당신은 20년 이상 홈페이지 개발 프로젝트를 진행하고 있는 웹기획자입니다." ID="ID_27564863" CREATED="1764899193437" MODIFIED="1764899213418"/>
+</node>
+<node TEXT="# 지시사항" ID="ID_1069816838" CREATED="1764899217703" MODIFIED="1764899223218">
+<node TEXT="모던한 느낌이면서 전문적으로 보이는 개인 포트폴리오 1페이지 웹사이트를 개발하려고 합니다.&#xa;포트폴리오 구성 요소는 일반적인 구성요소를 따라서 만들려고 합니다.&#xa;이러한 포트폴리오 페이지를 만들기 위한 상세 명세를 작성해서 개발하려고 합니다.&#xa;개발자에게 기능 구현 요청을 위한 기능 명세서를 핵심내용만 작성해주세요.&#xa;Mark Down 형식으로 정리해주세요." ID="ID_278415489" CREATED="1764899224095" MODIFIED="1764899878241"/>
+</node>
+</node>
+</node>
+<node TEXT="2. Antigravity에 구현 요청" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1961823542" CREATED="1764899055783" MODIFIED="1764900665249">
+<node TEXT="구현 요청 프롬프트" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_789283928" CREATED="1764899110273" MODIFIED="1764900674889">
+<node TEXT="#역할" ID="ID_994816780" CREATED="1764899122269" MODIFIED="1764899153930">
+<node TEXT="당신은 20년 이상 웹 서비스를 개발하는 개발자입니다." ID="ID_102380807" CREATED="1764899193437" MODIFIED="1764900732740"/>
+</node>
+<node TEXT="# 지시사항" ID="ID_1789838685" CREATED="1764899217703" MODIFIED="1764899223218">
+<node TEXT="지금까지 구현한 내용을 당신이 파악하기 위해서 전체 코드베이스를 읽어서 구현 내용을 파악해 주세요." ID="ID_647820946" CREATED="1764899224095" MODIFIED="1764900777787"/>
+</node>
+<node TEXT="# 상세지침" ID="ID_280693190" CREATED="1764899217703" MODIFIED="1764900820664">
+<node TEXT="파일의 요구사항 명세서를 참고해서 포트폴리오 웹사이틀 개발해주세요.&#xa;Layout과 배경색상, 타이포 중심으로 세련되게 개발해주시고, 이미지는 정말 필요한 부분만 적용해주세요.&#xa;배경 색상은 민트 색상톤으로 개발해주세요." ID="ID_1005880117" CREATED="1764899224095" MODIFIED="1764900914729"/>
+</node>
+</node>
+</node>
+<node TEXT="3. Github Repository연결" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" FOLDED="true" ID="ID_1047408231" CREATED="1764899055783" MODIFIED="1764908483079">
+<node TEXT="빌드 테스트&#xa;프롬프트" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_453895999" CREATED="1764899110273" MODIFIED="1764908723817">
+<node TEXT="배포를 위해서 build test를 진행해주세요." ID="ID_1826188301" CREATED="1764899122269" MODIFIED="1764908664919"/>
+<node TEXT="발생한 빌드 에러를 해결해주세요." ID="ID_868500916" CREATED="1764899217703" MODIFIED="1764908686383"/>
+<node TEXT="문제원인을 파악하고 조치해주세요." ID="ID_1636156348" CREATED="1764899217703" MODIFIED="1764908697733"/>
+</node>
+<node TEXT="빌드 배포" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_494453042" CREATED="1764899110273" MODIFIED="1764908760770">
+<node TEXT="git 사용자 설정" ID="ID_881458436" CREATED="1764899122269" MODIFIED="1764908840000">
+<node TEXT="git config --global user.email dhlee702@naver.com" ID="ID_482692424" CREATED="1764899193437" MODIFIED="1764908906319"/>
+</node>
+<node TEXT="git 사용자 설정" ID="ID_1449597779" CREATED="1764899122269" MODIFIED="1764908840000">
+<node TEXT="git config --global user.name dhlee702" ID="ID_269221128" CREATED="1764899193437" MODIFIED="1764908961896"/>
+</node>
+<node TEXT="git 초기화" ID="ID_655754627" CREATED="1764899122269" MODIFIED="1764909020584">
+<node TEXT="git init" ID="ID_1688844917" CREATED="1764899193437" MODIFIED="1764909029791"/>
+</node>
+<node TEXT="git commit -m &quot;First commit&quot;" ID="ID_1299711950" CREATED="1764909364206" MODIFIED="1764909366368"/>
+<node TEXT="git remote add origin https://github.com/dhlee702/portfolio_app.git" ID="ID_750761089" CREATED="1764909383014" MODIFIED="1764909385104"/>
+<node TEXT="git push -u origin master" ID="ID_1702367293" CREATED="1764909386140" MODIFIED="1764909405800"/>
+</node>
+</node>
+<node TEXT="4. Vercel 코드 배포와&#xa;외부 서비스로 공개하기" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_685585361" CREATED="1764899055783" MODIFIED="1764909507404">
+<node TEXT="빌드 테스트&#xa;프롬프트" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1461332410" CREATED="1764899110273" MODIFIED="1764908723817">
+<node TEXT="배포를 위해서 build test를 진행해주세요." ID="ID_744723833" CREATED="1764899122269" MODIFIED="1764908664919"/>
+<node TEXT="발생한 빌드 에러를 해결해주세요." ID="ID_1378473153" CREATED="1764899217703" MODIFIED="1764908686383"/>
+<node TEXT="문제원인을 파악하고 조치해주세요." ID="ID_509728220" CREATED="1764899217703" MODIFIED="1764908697733"/>
+</node>
+<node TEXT="빌드 배포" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1848189890" CREATED="1764899110273" MODIFIED="1764908760770">
+<node TEXT="git 사용자 설정" ID="ID_1154154728" CREATED="1764899122269" MODIFIED="1764908840000">
+<node TEXT="git config --global user.email dhlee702@naver.com" ID="ID_946169866" CREATED="1764899193437" MODIFIED="1764908906319"/>
+</node>
+<node TEXT="git 사용자 설정" ID="ID_488296250" CREATED="1764899122269" MODIFIED="1764908840000">
+<node TEXT="git config --global user.name dhlee702" ID="ID_143201162" CREATED="1764899193437" MODIFIED="1764908961896"/>
+</node>
+<node TEXT="git 초기화" ID="ID_1637566120" CREATED="1764899122269" MODIFIED="1764909020584">
+<node TEXT="git init" ID="ID_965289094" CREATED="1764899193437" MODIFIED="1764909029791"/>
+</node>
+<node TEXT="git commit -m &quot;First commit&quot;" ID="ID_185018615" CREATED="1764909364206" MODIFIED="1764909366368"/>
+<node TEXT="git remote add origin https://github.com/dhlee702/portfolio_app.git" ID="ID_1743856076" CREATED="1764909383014" MODIFIED="1764909385104"/>
+<node TEXT="git push -u origin master" ID="ID_1811569800" CREATED="1764909386140" MODIFIED="1764909405800"/>
+</node>
+</node>
+</node>
+</node>
+</map>
